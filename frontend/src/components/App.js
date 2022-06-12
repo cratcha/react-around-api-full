7ca66c1652ecc540301809648d6fe0a87e19d34e
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-06-12 19:49:58 -0400

    remove debugger

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -120,7 +120,6 @@ function App() {
     api
       .editUserInfo({ name, about })
       .then((newUserData) => {
-        debugger;
         setCurrentUser(newUserData);
         closeAllPopups();
       })
